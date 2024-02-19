308a22ccaa76df90591af74339bea689b8d6a504
ltphat2204 <ltphat2204@gmail.com>
2024-02-19 12:12:04 +0700

    Add post loading animation

---
+++ frontend/src/app/blogs/page.js
@@ -0,0 +1,9 @@
+import ListPosts from "@/components/ListPosts";
+
+export default function Blogs() {
+    return (
+        <div>
+            <ListPosts/>
+        </div>
+    )
+}
--- frontend/src/components/ListPosts.jsx
+++ frontend/src/components/ListPosts.jsx
@@ -1,10 +1,23 @@
 import PostCard from "./PostCard";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export default function ListPosts({classname, posts}) {
+    if (posts == undefined) return (
+        <div className="w-full h-full flex items-center justify-center">
+            <AiOutlineLoading3Quarters className="animate-spin ml-2 text-2xl"/>
+        </div>
+    )
+
+    if (posts.data == undefined || posts.data.length === 0) return (
+        <div className="w-full h-full flex items-center justify-center text-3xl">
+            There is no post now, come later!
+        </div>
+    )
+
     return (
         <div className={`${classname} flex flex-wrap w-full`}>
             {
-                posts && posts.data.map(post => <PostCard data={post} key={post.id}/>)
+                posts.data.map(post => <PostCard data={post} key={post.id}/>)
             }
         </div>
     );
